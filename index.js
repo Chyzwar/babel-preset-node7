f9869d4656b5b6b4e9f5421957495dcf743115fd
Chyzwar <schizek.marcin@gmail.com>
2016-12-28 08:03:22 +0000

    add system-import-transformer plugin

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: [
     require('babel-plugin-syntax-trailing-function-commas'),
     require('babel-plugin-transform-es2015-modules-commonjs'),
+    require('babel-plugin-system-import-transformer'),
     require('babel-plugin-syntax-async-functions'),
     require('babel-plugin-transform-class-properties'),
     require('babel-plugin-transform-object-rest-spread'),
